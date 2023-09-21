9ea62038f73edab62c0e14d405dd243d42f34d8a
sampath kumar <codyowl@gmail.com>
2023-09-21 15:36:16 +0530

    Objects added to base.js

--- base.js
+++ base.js
@@ -46,4 +46,18 @@ let myfirstarray = ['firstelement','secondelement'];
 /* accessing an array*/
 myfirstarray[0]
 myfirstarray[1]
+//===========================================================
+// Object
+let myobject = {};
+myobject['firstkey'] = 'firstvalue';
+myobject['secondkey'] = 'secondvalue';
+//console.log(myobject)
+// passing function as a property
+
+myobject['functionaskey'] = function(){console.log('text from function')};
+console.log(myobject)
+
+// calling function used inside object
+console.log(myobject.functionaskey());
+//=====================================================================
 
